e3c15ef45a449dd64edeaa03a3006ac7b464f186
Loli-Killer <38392948+Loli-Killer@users.noreply.github.com>
2022-10-19 17:46:01 -0400

    fix for new update

--- server/battle.py
+++ server/battle.py
@@ -5,7 +5,11 @@ def battleStart():
 
     data = request.data
     data = {
+        "apFailReturn": 0,
         'battleId': 'abcdefgh-1234-5678-a1b2c3d4e5f6',
+        "inApProtectPeriod": False,
+        "isApProtect": 0,
+        "notifyPowerScoreNotEnoughIfFailed": False,
         'playerDataDelta': {
             'modified': {},
             'deleted': {}
@@ -21,6 +25,18 @@ def battleFinish():
     data = request.data
     data = {
         "result":0,
+        "apFailReturn": 0,
+        "expScale": 1.2,
+        "goldScale": 1.2,
+        "rewards": [],
+        "firstRewards": [],
+        "unlockStages": [],
+        "unusualRewards": [],
+        "additionalRewards": [],
+        "furnitureRewards": [],
+        "alert": [],
+        "suggestFriend": False,
+        "pryResult": [],
         "playerDataDelta": {
             "modified": {},
             "deleted": {}
